e1c65710af0005476cb5bd104194286ae4640bdc
wyne1986 <366659539@qq.com>
2018-03-21 14:21:38 +0800

    Update my.php

--- module/config/my.php
+++ module/config/my.php
@@ -13,6 +13,6 @@ $config->default->lang   = 'zh-cn';
 
 /* 钉钉登录配置 */
 $config->ding->ddturnon = true;/* 是否开启钉钉登录 */
-$config->ding->appid = 'dingoaa6tty3uaxnifjqf7';/* 钉钉管理APPID */
-$config->ding->appsecret = 'xAtqU8deRLIue_mCrQ3LJ5Hz-F0BmQfgWn_CmJSw73d7WJta07datkpLk0wbr4YY';/* 钉钉密钥 */
-$config->ding->redirect = 'http://127.0.0.1/www/dingtalk-login.html';/* 回调地址,与钉钉管理后台保持一致 */
+$config->ding->appid = '';/* 钉钉管理APPID */
+$config->ding->appsecret = '';/* 钉钉密钥 */
+$config->ding->redirect = 'http://xxxxxxx/www/dingtalk-login.html';/* 回调地址,与钉钉管理后台保持一致 */
